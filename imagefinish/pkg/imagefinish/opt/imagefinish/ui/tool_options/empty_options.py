20e7aa52f9a2c462683638fd9cbd4f836de5653e
Lucia Lunadottir <Bordiyan20035@gmail.com>
2026-03-23 13:28:18 +0400

    Fix.

--- imagefinish/pkg/imagefinish/opt/imagefinish/ui/tool_options/empty_options.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import (QWidget, QHBoxLayout, QLabel)
-from .base_options import BaseOptions
-
-
-class EmptyOptions(BaseOptions):
-    def __init__(self, tr_key:str, parent=None):
-        super().__init__(parent)
-        self.layout.addWidget(self._lbl(tr_key))
-        self.layout.addStretch()
